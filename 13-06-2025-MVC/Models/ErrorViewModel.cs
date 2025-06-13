62d12801a8b972a4585c05245a67f6d65ad0bf9f
NguyenQuangBinhSteve <binhsteve123@gmail.com>
2025-06-13 08:40:08 +0700

    Commit 13-06-2025-MVC

---
+++ 13-06-2025-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _13_06_2025_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
