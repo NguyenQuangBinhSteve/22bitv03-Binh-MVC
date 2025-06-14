55f979aa4f7996e2f236544dbc4b673f1112fe0b
NguyenQuangBinhSteve <binhsteve123@gmail.com>
2025-06-14 10:42:25 +0700

    14-06-2025-MVC

---
+++ 13-06-2025-MVC/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using _13_06_2025_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class Democontroller : Controller
+{
+	// host /Demo/ AAA
+	public IActionResult AAA()
+	{
+		return Content("OK");
+	}
+
+    // host /Demo/ BBB
+	public IActionResult BBB()
+	{
+		return Content("OK BBB");
+	}
+
+	public string ActionTest()
+	{
+		return "ActionTest OK";
+    }
+}
+
